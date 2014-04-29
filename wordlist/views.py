ea2e6024f4a804473cb866b0d91a32e5e2f9218b
Karol Wach <karol@wach.me>
2014-04-29 02:26:30 +0200

    Created PartialUpdateMixin that supports partial updates via HTTP PATCH request

--- wordlist/views.py
+++ wordlist/views.py
@@ -2,29 +2,33 @@ from restless import modelviews as views
 from models import *
 from restless.models import serialize
 
-class WordlistList(views.ListEndpoint):
-    model = Wordlist
-
-class WordlistDetail(views.DetailEndpoint):
-    model = Wordlist
-    
+class PartialUpdateMixin(object):
     def fill_data(self, request, *args, **kwargs):
         instance = self.get_instance(self, request, *args, **kwargs)
         request.data = dict(serialize(instance), **request.data)
         
-    def put(self, request, *args, **kwargs):
+    def patch(self, request, *args, **kwargs):
         self.fill_data(request, *args, **kwargs)
-        return super(WordlistDetail, self).put(request, *args, **kwargs)
+        return super(PartialUpdateMixin, self).put(request, *args, **kwargs)
     
+# Wordlist views
+    
+class WordlistList(views.ListEndpoint):
+    model = Wordlist
+
+class WordlistDetail(PartialUpdateMixin, views.DetailEndpoint):
+    model = Wordlist
+
     def serialize(self, obj):
         return serialize(obj, include=[ ('words', lambda o : serialize(o.word_set)) ])
     
+# Word views
 
 class WordCreate(views.ListEndpoint):
     model = Word
     methods = ['POST']
 
-class WordUpdate(views.DetailEndpoint):
+class WordUpdate(PartialUpdateMixin, views.DetailEndpoint):
     model = Word
     methods = ['PUT', 'DELETE']
 
